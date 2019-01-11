3ad453e2b19eb50a3069cde650ddeb64e77c0857
ch0mper <hannahchang17@gmail.com>
2019-01-11 16:53:29 -0600

    react native lesson

---
+++ screens/MotionScreen.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Text, View, TouchableOpacity } from 'react-native'
+import { DangerZone } from 'expo'
+
+const { DeviceMotion } = DangerZone
+
+export default class CameraScreen extends React.Component {
+
+    state = {
+        rotation: {
+            alpha: 0, 
+            beta: 0,
+            gamma: 0
+        }
+    }
+
+    componentDidMount(){
+        DeviceMotion.addListener( ({ rotation }) => {
+            this.setState({ rotation })
+        })
+    }
+
+    render() {
+        const { alpha, beta, gamma } = this.state.rotation
+        return(
+            <View style={{flex:1}}>
+                <Text style={{fontSize:30}}>Alpha: {alpha}</Text>
+                <Text style={{fontSize:30}}>Beta: {beta}</Text>
+                <Text style={{fontSize:30}}>Gamma: {gamma}</Text>
+            </View>
+        )
+    }
+
+}
